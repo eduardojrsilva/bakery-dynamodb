5f74c41653bdc5f58c49b66317aa16afdedfe1b3
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2023-07-05 10:24:21 -0300

    adding suppliers CRUD

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -4,4 +4,10 @@ module.exports = {
   unitsFindAll: require('./units/findAll.js'),
   unitsUpdate: require('./units/update.js'),
   unitsDelete: require('./units/delete.js'),
+
+  suppliersCreate: require('./suppliers/create.js'),
+  suppliersFindById: require('./suppliers/findById.js'),
+  suppliersFindAll: require('./suppliers/findAll.js'),
+  suppliersUpdate: require('./suppliers/update.js'),
+  suppliersDelete: require('./suppliers/delete.js'),
 }
---
+++ src/handlers/suppliers/create.js
@@ -0,0 +1,62 @@
+const Joi = require('joi');
+
+const DatabaseProvider = require('../../providers/database');
+const decoratorValidator = require('../../util/decoratorValidator');
+const globalEnum = require('../../util/globalEnum');
+
+class Handler {
+  constructor(){
+    this.database = new DatabaseProvider('Suppliers');
+  }
+
+  static validator() {
+    return Joi.object({
+      cnpj: Joi.string().required(),
+      name: Joi.string().required(),
+    });
+  }
+
+  handlerSuccess(data) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+
+    return response;
+  }
+
+  handlerError(data) {
+    const response = {
+      statusCode: data.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: JSON.stringify({error: "Couldn't create item!"})
+    }
+
+    return response;
+  }
+
+  async main(event) {
+    try {
+      const data = event.body;
+
+      // verify cnpj
+
+      const supplier = await this.database.create(data);
+
+      return this.handlerSuccess(supplier);
+    } catch (error) {
+      console.log('Erro *** ', error.stack);
+
+      return this.handlerError({ statusCode: 500 });
+    }
+  }
+}
+
+
+const handler = new Handler();
+
+module.exports = decoratorValidator(
+  handler.main.bind(handler),
+  Handler.validator(),
+  globalEnum.ARG_TYPE.BODY
+);
