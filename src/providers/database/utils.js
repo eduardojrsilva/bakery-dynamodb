3e4894c9170137344712dc29efc6a4202953e278
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2023-08-28 10:21:24 -0300

    removing unnused utils

--- src/providers/database/utils.js
+++ src/providers/database/utils.js
@@ -3,51 +3,10 @@ const AWS = require('aws-sdk');
 const FIRST_ITEM_INDEX = 0;
 const SECOND_ITEM_INDEX = 1;
 
-async function verifyIfExistsInTable(TableName, id) {
-  const dynamoDB = new AWS.DynamoDB.DocumentClient({ params: { TableName } });
-
-  const { Items: allItems } = await dynamoDB.scan().promise();
-
-  const item = allItems.find((item) => item.id === id && item.active);
-
-  return !!item;
-}
-
-function verifyIfExistsInList(items, id) {
-  const item = items.find((item) => item.id === id);
-
-  return !!item;
-}
-
 function removeActiveProperty(item) {
   const { active, ...props } = item;
 
   return { ...props };
-}
-
-async function getRelations({ joinTable, itemId, itemSide = 'left' }) {
-  if (itemSide !== 'left' && itemSide !== 'right') return;
-
-  const [keyIndex, relationIndex] = itemSide === 'left' ? [FIRST_ITEM_INDEX, SECOND_ITEM_INDEX] : [SECOND_ITEM_INDEX, FIRST_ITEM_INDEX];
-
-  const joinTableDatabase = new AWS.DynamoDB.DocumentClient({ params: { TableName: joinTable } });
-
-  const { Items: allItems } = await joinTableDatabase.scan().promise();
-
-  const activeItems = allItems.filter(({ active }) => active);
-
-  const relations = activeItems.filter(({ id }) => id.split('#')[keyIndex] === itemId);
-
-  const transformed = relations.map((item) => {
-    const id = item.id.split('#')[relationIndex];
-
-    return {
-      ...item,
-      id,
-    };
-  });
-
-  return transformed;
 }
 
 async function writePutTransaction(transactionsItems) {
@@ -69,4 +28,4 @@ async function writePutTransaction(transactionsItems) {
   }).promise();
 }
 
-module.exports = { verifyIfExistsInTable, verifyIfExistsInList, removeActiveProperty, getRelations, writePutTransaction };
+module.exports = { removeActiveProperty, writePutTransaction };
